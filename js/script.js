6513db656c5893458d4de0bc9972159ba5be43d8
Alessandro Venti <alessandro.venti@icloud.com>
2021-04-08 14:23:42 +0200

    html-css done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
                        //Recuperando quanto visto in classe, generare il js per lo slider, con l'obbiettivo di comprendere cosa sta succedendo.
 
 // $(document).ready(lollipop);
-console.log('hello');
-
