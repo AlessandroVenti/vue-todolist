2d50b1644017cb19c29b8f9ae3dc26ae01db5655
Alessandro Venti <alessandro.venti@icloud.com>
2021-04-08 13:35:14 +0200

    Setup project

---
+++ js/script.js
@@ -0,0 +1,5 @@
+                       //Recuperando quanto visto in classe, generare il js per lo slider, con l'obbiettivo di comprendere cosa sta succedendo.
+
+// $(document).ready(lollipop);
+console.log('hello');
+
